97fd0d33aa5e4ef40f523b53b661c720a2294c30
Raghu Rana <raghurana@gmail.com>
2022-02-24 10:49:41 +1100

    email logo is displayed

--- src/components/House.tsx
+++ src/components/House.tsx
@@ -1,5 +1,6 @@
 import "./House.css";
 import { House } from "../interfaces";
+import emailIcon from "../assets/images/Email.png";
 
 const HouseComponent = (props: { house: House }) => {
   if (!props.house) return <div>Loading...</div>;
@@ -9,7 +10,11 @@ const HouseComponent = (props: { house: House }) => {
       <div className="address">{props.house.address}</div>
       <div className="even-columns">
         <img src={`/images/${props.house.photo}.jpeg`} alt="House" />
-        <div className="house-desc">{props.house.description}</div>
+        <div className="house-desc">
+          <div className="priceTag">${props.house.price}</div>
+          <div>{props.house.description}</div>
+          <img src={emailIcon} alt="inquiry" />
+        </div>
       </div>
     </div>
   );
