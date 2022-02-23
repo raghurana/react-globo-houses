6defe9400cd4def5036dc24e3c325933082a8a2c
Raghu Rana <raghurana@gmail.com>
2022-02-23 14:21:21 +1100

    fetch data is working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,11 @@ import "./App.css";
 import Header from "./Header";
 import House from "./House";
 import HouseFilter from "./HouseFilter";
+import { useHouses } from "../hooks";
 
 function App() {
+  const allHouses = useHouses();
+  console.log(allHouses);
   return (
     <div className="app-container">
       <Header title="Providing houses all over the world"></Header>
---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useHouses } from "./useHouses";
